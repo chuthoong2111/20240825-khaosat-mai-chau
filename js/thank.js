a55fbcfdb80252da7cdae5858d9f14f93e31aede
chuthoong2111 <chuthoong2111@gmail.com>
2024-09-10 22:42:17 +0700

    update footer + thank page

--- js/thank.js
+++ js/thank.js
@@ -1,12 +1,16 @@
-var myCanvas = document.createElement('canvas');
+var myCanvas = document.createElement("canvas");
 
 var myConfetti = confetti.create(myCanvas, {
   resize: true,
-  useWorker: true
+  useWorker: true,
 });
-myConfetti({
+confetti({
   particleCount: 100,
-  spread: 160
-  // any other options from the global
-  // confetti function
+  startVelocity: 30,
+  spread: 360,
+  origin: {
+    x: Math.random(),
+    // since they fall down, start a bit higher than random
+    y: Math.random() - 0.2,
+  },
 });
