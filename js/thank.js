00cc1790e0c83212eea2807805c26b11291c45a6
long.dongoc <long.dongoc@cybereye.vn>
2024-09-10 20:55:38 +0700

    thank page

---
+++ js/thank.js
@@ -0,0 +1,12 @@
+var myCanvas = document.createElement('canvas');
+
+var myConfetti = confetti.create(myCanvas, {
+  resize: true,
+  useWorker: true
+});
+myConfetti({
+  particleCount: 100,
+  spread: 160
+  // any other options from the global
+  // confetti function
+});
